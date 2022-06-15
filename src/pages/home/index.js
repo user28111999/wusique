427610e555377b0d48ee28814c51d40916628163
Noel <28111999@protonmail.com>
2022-06-16 00:52:40 +0200

    added fa-icons+more ideas

--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -1,4 +1,8 @@
+// todos:
+// split each item into a component
 import styled from "styled-components"
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
+import { faChevronRight, faSearch } from "@fortawesome/free-solid-svg-icons"
 
 const Container = styled.div`
 	width: 92.5%;
@@ -14,6 +18,10 @@ const Item = styled.div`
 	display: flex;
 	flex-direction: column;
 	margin: 8px 0;
+
+	&:last-child {
+		padding-bottom: 2rem;
+	}
 `
 
 const ItemTitle = styled.h2`
@@ -113,10 +121,62 @@ const SearchContainer = styled.div`
 	}
 `
 
+const IconWrapper = styled.div`
+	position: absolute;
+	padding: 8px;
+	right: 1.15rem;
+`
+
+const ListWrapper = styled.div`
+	width: 100%;
+	display: flex;
+	flex-direction: column;
+	margin: 1rem 0;
+`
+
+const ListItem = styled.div`
+	display: flex;
+	justify-content: space-between;
+	border-top: 1px solid #fff;
+	padding: 12px;
+	opacity: .75;
+
+	&:last-child {
+		border-bottom: 1px solid #fff;
+	}
+`
+
+const ListItemTitle = styled.p`
+	font-family: "Montserrat", sans-serif;
+	font-size: 0.85rem;
+	font-weight: 600;
+	white-space: nowrap;
+	overflow: hidden;
+`
+
+const Footer = styled.div`
+	display: flex;
+	border-top: 1px solid #fff;
+	justify-content: center;
+	margin-top: 2rem;
+	padding-top: 0.5rem;
+	padding-bottom: 1.5rem;
+	width: 90%;
+	align-self: center;
+	font-family: "Montserrat", sans-serif;
+	font-size: .8rem;
+	opacity: .5;
+`
+
 const Home = () => {
 	return (
 		<Container>
 			<SearchContainer>
+				<IconWrapper>
+					<FontAwesomeIcon
+						icon={faSearch}
+					/>
+				</IconWrapper>
 				<input 
 					placeholder="Search..."
 				/>
@@ -253,7 +313,94 @@ const Home = () => {
 			</Item>
 			<Item>
 				<ItemTitle>Library</ItemTitle>
+				<ListWrapper>
+					<ListItem>
+						<ListItemTitle>Artists</ListItemTitle>
+						<FontAwesomeIcon icon={faChevronRight} />
+					</ListItem>
+					<ListItem>
+						<ListItemTitle>Tags</ListItemTitle>
+						<div>
+							<FontAwesomeIcon icon={faChevronRight} />
+						</div>
+					</ListItem>
+					<ListItem>
+						<ListItemTitle>Playlists</ListItemTitle>
+						<div>
+							<FontAwesomeIcon icon={faChevronRight} />
+						</div>
+					</ListItem>
+					<ListItem>
+						<ListItemTitle>Settings</ListItemTitle>
+						<div>
+							<FontAwesomeIcon icon={faChevronRight} />
+						</div>
+					</ListItem>
+				</ListWrapper>
 			</Item>
+			<Item>
+				<ItemTitle>NEAR YOU</ItemTitle>
+				<AlbumWrapper>
+					<Album>
+						<Cover src="https://external-content.duckduckgo.com/iu/?u=http%3A%2F%2Fwww.copertinedvd.org%2Fcopertine-cd-file%2FA%2Faphex_twin_-_selected_ambient_works_volume_ii_-_front.jpg&f=1&nofb=1" alt />
+						<AlbumTitle>Daft Punk</AlbumTitle>
+						<div style={{
+							display: "flex",
+							justifyContent: "space-between"
+						}}>
+							<AlbumTags>Paris, FR</AlbumTags>
+							<AlbumYear>18/08/2027</AlbumYear>
+						</div>
+					</Album>
+					<Album>
+						<Cover src="https://external-content.duckduckgo.com/iu/?u=http%3A%2F%2Fwww.copertinedvd.org%2Fcopertine-cd-file%2FA%2Faphex_twin_-_selected_ambient_works_volume_ii_-_front.jpg&f=1&nofb=1" alt />
+						<AlbumTitle>BTS</AlbumTitle>
+						<div style={{
+							display: "flex",
+							justifyContent: "space-between"
+						}}>
+							<AlbumTags>Paris, FR</AlbumTags>
+							<AlbumYear>18/08/2027</AlbumYear>
+						</div>
+					</Album>
+					<Album>
+						<Cover src="https://external-content.duckduckgo.com/iu/?u=http%3A%2F%2Fwww.copertinedvd.org%2Fcopertine-cd-file%2FA%2Faphex_twin_-_selected_ambient_works_volume_ii_-_front.jpg&f=1&nofb=1" alt />
+						<AlbumTitle>SOPHIE</AlbumTitle>
+						<div style={{
+							display: "flex",
+							justifyContent: "space-between"
+						}}>
+							<AlbumTags>Heaven</AlbumTags>
+							<AlbumYear>20/04/2023</AlbumYear>
+						</div>
+					</Album>
+					<Album>
+						<Cover src="https://external-content.duckduckgo.com/iu/?u=http%3A%2F%2Fwww.copertinedvd.org%2Fcopertine-cd-file%2FA%2Faphex_twin_-_selected_ambient_works_volume_ii_-_front.jpg&f=1&nofb=1" alt />
+						<AlbumTitle>Jeff Mills</AlbumTitle>
+						<div style={{
+							display: "flex",
+							justifyContent: "space-between"
+						}}>
+							<AlbumTags>Detroit, MI</AlbumTags>
+							<AlbumYear>18/08/2027</AlbumYear>
+						</div>
+					</Album>
+					<Album>
+						<Cover src="https://external-content.duckduckgo.com/iu/?u=http%3A%2F%2Fwww.copertinedvd.org%2Fcopertine-cd-file%2FA%2Faphex_twin_-_selected_ambient_works_volume_ii_-_front.jpg&f=1&nofb=1" alt />
+						<AlbumTitle>Biosphere</AlbumTitle>
+						<div style={{
+							display: "flex",
+							justifyContent: "space-between"
+						}}>
+							<AlbumTags>Manchester, UK</AlbumTags>
+							<AlbumYear>18/08/2027</AlbumYear>
+						</div>
+					</Album>
+				</AlbumWrapper>
+			</Item>
+			<Footer>
+				v0.1.0 - © 2022 user28111999
+			</Footer>
 		</Container>
 	)
 }
